f2e15d189d70ca7bf4208b0ea74c13d0c5b95fcb
Abdu Ameen <abdu.ameen000@gmail.com>
2020-09-03 20:05:15 -0700

    added beginning website

--- components/projectcard.tsx
+++ components/projectcard.tsx
@@ -1,34 +1,47 @@
 import React from "react";
 import { ChevronRight } from "react-feather";
+import { Card, CardContent, makeStyles, ButtonBase } from "@material-ui/core";
 
-const ProjectCard: React.FunctionComponent = () => (
-  <>
-    <div className="projectrect">
-      <text className="subtitle1">Project Title</text>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-        }}
-      >
-        <p className="subtitle2">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum
-        </p>
-        <div className="arrow">
-          <ChevronRight />
-        </div>
-      </div>
-    </div>
-  </>
-);
+interface ProjectCardProps {
+  title: string;
+  content: string;
+  url: string;
+}
+
+const ProjectCard: React.FunctionComponent<ProjectCardProps> = ({
+  title,
+  content,
+  url,
+}: ProjectCardProps) => {
+  return (
+    <>
+      <Card variant="outlined">
+        <ButtonBase
+          style={{ display: "block", textAlign: "initial" }}
+          onClick={(event) => {
+            window.location.href = url;
+          }}
+        >
+          <CardContent>
+            <text className="subtitle1">{title}</text>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                // justifyContent: "center",
+              }}
+            >
+              <p className="subtitle2">{content}</p>
+              <div className="arrow">
+                <ChevronRight />
+              </div>
+            </div>
+          </CardContent>
+        </ButtonBase>
+      </Card>
+    </>
+  );
+};
 
 export default ProjectCard;
