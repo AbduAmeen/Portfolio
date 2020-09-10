652f27d75bf2f9821525fdceda801710f51e0521
Abdu Ameen <abdu.ameen000@gmail.com>
2020-09-09 23:29:35 -0700

    added nav and mobile support

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,6 +24,7 @@ const Index: React.FunctionComponent = () => (
           Abdu
         </text>
         <div
+          id="hambutton"
           className="hambutton"
           onClick={() => {
             var navbuttons = document.getElementById("navbuttons");
@@ -31,8 +32,6 @@ const Index: React.FunctionComponent = () => (
               navbuttons.style.display = "none";
             else {
               navbuttons.style.display = "flex";
-              document.getElementById("main").style.paddingTop +=
-                navbuttons.offsetHeight;
             }
           }}
         >
@@ -40,7 +39,16 @@ const Index: React.FunctionComponent = () => (
         </div>
       </div>
 
-      <div id="navbuttons" className="navbuttons">
+      <div
+        id="navbuttons"
+        className="navbuttons"
+        onBlur={(event) => {
+          var navbuttons = event.target;
+          if (navbuttons.style.display == "flex")
+            navbuttons.style.display = "none";
+        }}
+        tabIndex={1}
+      >
         <a className="caption active" href="#landingpage">
           Home
         </a>
@@ -55,25 +63,37 @@ const Index: React.FunctionComponent = () => (
         </a>
       </div>
     </div>
-    <div id="main" className="main">
-      <div id="landingpage" className="landingpage">
-        <div style={{ display: "flex", flexDirection: "column" }}>
-          <text className="h1 bold">
-            Hello world,
-            <br /> I am Abd-Alrahman Ameen
-          </text>
-          <text className="st">
-            I am a developer interested in improving the world with the power of
-            software
-          </text>
+    <div
+      className="content"
+      onClick={() => {
+        var navbuttons = document.getElementById("navbuttons");
+        if (
+          window.getComputedStyle(navbuttons).display == "flex" &&
+          window.getComputedStyle(document.getElementById("hambutton"))
+            .display != "none"
+        )
+          navbuttons.style.display = "none";
+      }}
+    >
+      <div id="main" className="main">
+        <div id="landingpage" className="landingpage">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <text className="h1 bold">
+              Hello world,
+              <br /> I am Abd-Alrahman Ameen
+            </text>
+            <text className="st">
+              I am a developer interested in improving the world with the power
+              of software
+            </text>
+          </div>
+          <div className="mainimg">
+            <img src="mounthood.jpg" />
+            <text className="grey">Mount Hood, Oregon</text>
+          </div>
         </div>
-        <div className="mainimg">
-          <img src="mounthood.jpg" />
-          <text className="grey">Mount Hood, Oregon</text>
-        </div>
-      </div>
 
-      {/* <div className="rects">
+        {/* <div className="rects">
         <div className="rect color1" />
         <div className="rect color2" />
         <div className="rect color3" />
@@ -84,104 +104,104 @@ const Index: React.FunctionComponent = () => (
         />
       </div> */}
 
-      <div id="whoiam" className="whoiam">
-        <text className="title h3 bold">Who I Am</text>
-        <p className="content">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
-      </div>
-      <div id="skills" className="skills">
-        <text className="h3 bold">Skills</text>
-        <div className="skillcards">
-          <SkillCard
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book."
-            imgsrc="cpp.svg"
-          />
-          <SkillCard
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book."
-            imgsrc="js.svg"
-          />
-          <SkillCard
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book."
-            imgsrc="html.svg"
-          />
+        <div id="whoiam" className="whoiam">
+          <text className="title h3 bold">Who I Am</text>
+          <p className="content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
         </div>
-      </div>
-      <div id="project" className="project">
-        <text className="h3 bold">Projects</text>
-        <div className="projects">
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
-          <ProjectCard
-            title="Project Title"
-            content="Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has"
-            url="https://github.com/AbduAmeen/Website-Blocker"
-          />
+        <div id="skills" className="skills">
+          <text className="h3 bold">Skills</text>
+          <div className="skillcards">
+            <SkillCard
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+              imgsrc="cpp.svg"
+            />
+            <SkillCard
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+              imgsrc="js.svg"
+            />
+            <SkillCard
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book."
+              imgsrc="html.svg"
+            />
+          </div>
         </div>
-      </div>
+        <div id="project" className="project">
+          <text className="h3 bold">Projects</text>
+          <div className="projects">
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+            <ProjectCard
+              title="Project Title"
+              content="Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has"
+              url="https://github.com/AbduAmeen/Website-Blocker"
+            />
+          </div>
+        </div>
 
-      <div id="contact" className="contact">
-        <text className="h3 bold">Contact</text>
-        <div className="emailbox">
-          <text className="st grey">Have an offer or want to chat?</text>
+        <div id="contact" className="contact">
+          <text className="h3 bold">Contact</text>
+          <div className="emailbox">
+            <text className="st grey">Have an offer or want to chat?</text>
 
-          <text className="h4 bold email">abdu.ameen000@gmail.com</text>
-        </div>
-        {/* <div className="single"> */}
-        {/* <TextField
+            <text className="h4 bold email">abdu.ameen000@gmail.com</text>
+          </div>
+          {/* <div className="single"> */}
+          {/* <TextField
             variant="outlined"
             required
             label="Your name"
@@ -208,7 +228,8 @@ const Index: React.FunctionComponent = () => (
             multiline
             fullWidth
           /> */}
-        {/* </div> */}
+          {/* </div> */}
+        </div>
       </div>
     </div>
     {/* <img src="/drawing.svg" alt="caret" key={Date.now()} className="caret" /> */}
